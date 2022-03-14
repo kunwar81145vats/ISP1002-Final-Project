6c64a29aa1b998cbac8269674121f892a3de850e
kunwar81145vats <97743194+kunwar81145vats@users.noreply.github.com>
2022-03-14 06:58:10 +0530

    Initial commit: 1. Project created 2. Tab controller created in the storyboard. 3. Images added in assets for tab bar icons.

---
+++ InstaEat/InstaEat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstaEat
+//
+//  Created by Kunwar Vats on 14/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
