25113a26a746dc619376d306cf5cb8643aa1cf70
kunwar81145vats <97743194+kunwar81145vats@users.noreply.github.com>
2022-03-14 07:13:34 +0530

    Commit details: 1. Basic structure created for the project. 2. ViewController classes added for all the app screens. 3. Common class created to contain common methods to be used throughout the app. 4. Constants file created to store all the constants to be used in the app.

---
+++ InstaEat/InstaEat/Common/Common.swift
@@ -0,0 +1,12 @@
+//
+//  Common.swift
+//  InstaEat
+//
+//  Created by Kunwar Vats on 14/03/22.
+//
+
+import UIKit
+
+class Common: NSObject {
+
+}
---
+++ InstaEat/InstaEat/Common/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  InstaEat
+//
+//  Created by Kunwar Vats on 14/03/22.
+//
+
+import Foundation
